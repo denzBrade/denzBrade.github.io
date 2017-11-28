35545760267d3ed251123fbadc444525a259b52c
Denzil Brade <denzbrade@gmail.com>
2017-11-28 19:33:31 +0000

    JS file added

--- biglight.js
+++ biglight.js
@@ -10,7 +10,7 @@ function addTitle() {
 }
 
 window.onload = function () {
-    let filterContainer = document.querySelector(".filters-nav")[0];
+    let filterContainer = document.querySelectorAll(".filters-nav")[0];
     console.log(filterContainer);
     console.log('HERE IS MY SCRIPT')  
 }
