2374eb993e5c33034e33e54fbfddaebc01e7868d
Denzil Brade <denzbrade@gmail.com>
2017-11-28 23:41:04 +0000

    filter title added

--- biglight.js
+++ biglight.js
@@ -1,26 +1,29 @@
-// Insert title above filters 
-function addTitle() {
+// // Insert title above filters 
+// function addTitle() {
     
-    //let filterTitle = document.createElement('h2');
-    //let filterTitleText = document.createTextNode('Filter')
-    //console.log(filterContainer);
-    //filterTitle.insertBefore(filterTitleText, filterTitle.lastChild);
-    //filterContainer.insertAfter(filterTitle, filterContainer.lastChild);
-    //filterTitle.setAttribute('class', 'filter-main-title');
-}
+//     //let filterTitle = document.createElement('h2');
+//     //let filterTitleText = document.createTextNode('Filter')
+//     //console.log(filterContainer);
+//     //filterTitle.insertBefore(filterTitleText, filterTitle.lastChild);
+//     //filterContainer.insertAfter(filterTitle, filterContainer.lastChild);
+//     //filterTitle.setAttribute('class', 'filter-main-title');
+// }
 
-window.onload = function () {
+// window.onload = function () {
     
-    console.log('HERE IS MY SCRIPT')
+function addTitle() {
+    console.log('HERE IS MY SCRIPT');
     
     let filterNav = document.querySelector("#nav-dept");
     let filterTitle = document.createElement('h2');
-    let filterTitleText = document.createTextNode('Filter')
+    let filterTitleText = document.createTextNode('Filter');
 
 
     filterTitle.className = 'filter-title-main';
     filterTitle.appendChild(filterTitleText);
 
-    filterNav.insertBefore(filterTitle, filterNav.firstChild);  
+    filterNav.insertBefore(filterTitle, filterNav.firstChild);
 }
+      
+// }
 
