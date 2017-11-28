c6b4ddd72134ccf9a18045a8b4859f61f00e5364
Denzil Brade <denzbrade@gmail.com>
2017-11-28 18:46:37 +0000

    JS file added

---
+++ biglight.js
@@ -0,0 +1,15 @@
+// Insert title above filters 
+function addTitle() {
+    
+    //let filterTitle = document.createElement('h2');
+    //let filterTitleText = document.createTextNode('Filter')
+    //console.log(filterContainer);
+    //filterTitle.insertBefore(filterTitleText, filterTitle.lastChild);
+    //filterContainer.insertAfter(filterTitle, filterContainer.lastChild);
+    //filterTitle.setAttribute('class', 'filter-main-title');
+}
+
+window.onload = function () {
+    let filterContainer = document.querySelector("#fsm-filters-container");
+    console.log(filterContainer);  
+}
