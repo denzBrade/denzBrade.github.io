347086fc6c3f29cd90b1b6f65487d17b0f13aeae
Denzil Brade <denzbrade@gmail.com>
2017-11-29 01:31:25 +0000

    add buttons

--- biglight.js
+++ biglight.js
@@ -1,18 +1,5 @@
-// // Insert title above filters 
-// function addTitle() {
-    
-//     //let filterTitle = document.createElement('h2');
-//     //let filterTitleText = document.createTextNode('Filter')
-//     //console.log(filterContainer);
-//     //filterTitle.insertBefore(filterTitleText, filterTitle.lastChild);
-//     //filterContainer.insertAfter(filterTitle, filterContainer.lastChild);
-//     //filterTitle.setAttribute('class', 'filter-main-title');
-// }
-
-// window.onload = function () {
-    
-function addTitle() {
-    console.log('HERE IS MY SCRIPT');
+// Create & Add Filter Title to DOM
+function addFilterTitle() {
     
     let filterNav = document.querySelector("#nav-dept");
     let filterTitle = document.createElement('h2');
@@ -21,11 +8,20 @@ function addTitle() {
 
     filterTitle.className = 'filter-title-main';
     filterTitle.appendChild(filterTitleText);
-
     filterNav.insertBefore(filterTitle, filterNav.firstChild);
 } 
 
-addTitle();
+function addDoneButton() {
+    let filterNav = document.querySelector("#nav-dept");
+    let doneButton = document.createElement('button');
+
+    doneButton.innerHTML = "Done";
+    doneButton.className = 'filter-button-done';
+    filterNav.insertAfter(doneButton, filterNav.firstChild);
+}
+
+addFilterTitle();
+addDoneButton();
       
-// }
+
 
