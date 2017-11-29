57ce27197a804bb9ba6d0729b4f62376814ebdc0
Denzil Brade <denzbrade@gmail.com>
2017-11-29 01:49:24 +0000

    add buttons

--- biglight.js
+++ biglight.js
@@ -1,27 +1,38 @@
 // Create & Add Filter Title to DOM
+let filterSection = document.querySelector("#nav-dept");
+
 function addFilterTitle() {
     
-    let filterNav = document.querySelector("#nav-dept");
+    // let filterNav = document.querySelector("#nav-dept");
     let filterTitle = document.createElement('h2');
     let filterTitleText = document.createTextNode('Filter');
 
-
     filterTitle.className = 'filter-title-main';
     filterTitle.appendChild(filterTitleText);
-    filterNav.insertBefore(filterTitle, filterNav.firstChild);
+    filterSection.insertBefore(filterTitle, filterSection.firstChild);
 } 
 
 function addDoneButton() {
-    let filterNav = document.querySelector("#nav-dept");
+    // let filterNav = document.querySelector("#nav-dept");
     let doneButton = document.createElement('button');
 
     doneButton.innerHTML = "Done";
     doneButton.className = 'filter-button-done';
-    filterNav.insertBefore(doneButton, filterNav.firstChild);
+    filterSection.insertBefore(doneButton, filterSection.firstChild);
+}
+
+function addResetButton() {
+    // let filterNav = document.querySelector("#nav-dept");
+    let resetButton = document.createElement('button');
+
+    resetButton.innerHTML = "Reset";
+    resetButton.className = 'filter-button-reset';
+    filterSection.insertBefore(resetButton, filterSection.firstChild);
 }
 
 addFilterTitle();
 addDoneButton();
+addResetButton();
       
 
 
